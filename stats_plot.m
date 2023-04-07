6e2dc8051a37095aea2e792372fb6c42dd709291
joshuaning <joshning@umich.edu>
2023-04-06 23:56:04 -0400

    deepend convolutional layers, modified code for GPU

--- stats_plot.m
+++ stats_plot.m
@@ -3,4 +3,8 @@ clc;
 clf;
 
 stats = csvread("stats.csv");
-plot(stats(:,1), stats(:,3)./stats(:,4)); 
+plot(stats(:,1), stats(:,3)./stats(:,4), 'linewidth', 2); 
+
+xlabel('Number of Batches(n)');
+ylabel('Running Accuracy');
+title('Running Accuracy of VGG-BN in 3 Epochs');
